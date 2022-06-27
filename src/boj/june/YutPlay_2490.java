9166183f984a897691c74c0641cddc1db25f72ff
jlal1226 <jlal1226@naver.com>
2022-06-28 00:48:59 +0900

    [boj] - 2490

---
+++ src/boj/june/YutPlay_2490.java
@@ -0,0 +1,43 @@
+package boj.june;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class YutPlay_2490 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            while (st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+
+            switch (sum) {
+                case 0:
+                    System.out.println("D");
+                    break;
+                case 1:
+                    System.out.println("C");
+                    break;
+                case 2 :
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("A");
+                    break;
+                case 4:
+                    System.out.println("E");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
